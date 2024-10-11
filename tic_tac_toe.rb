093dabbe00e62277306d0e267c02efc9ec328e03
TerrazGitLinux <rafaelalejandroterrazas@hotmail.com>
2024-10-11 12:15:03 -0300

    Diagonal Checker added

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -34,8 +34,12 @@ end
 
 module WinConditions
   def diagonal_checker(player)
-    self.board.container.all? do |row|
-    end
+    #main diagonal = [0][0], [1][1], [2][2]
+    main_diagonal = (0..2).all? {|i| self.board.container[i][i] == player.player_mark}
+    #anti_diagonal = [0][2], [1][1], [2][0]
+    anti_diagonal = (0..2).all? {|i| self.board.container[i][2-i] == player.player_mark}
+
+    main_diagonal || anti_diagonal
   end
 
   def horizontal_checker(player)
@@ -51,7 +55,7 @@ module WinConditions
   end
   
   def somebody_won?
-    if self.horizontal_checker(self.player_1) || self.horizontal_checker(self.player_2) || self.vertical_checker(self.player_1) || self.vertical_checker(self.player_2)
+    if self.horizontal_checker(self.player_1) || self.horizontal_checker(self.player_2) || self.vertical_checker(self.player_1) || self.vertical_checker(self.player_2) || diagonal_checker(self.player_1) || diagonal_checker(player_2)
       true
     else
       false
